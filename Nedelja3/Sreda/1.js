d3305ac14995464ecc759576ba3dce48cbc74e6d
SuzanaRadovanovic <68377745+SuzanaRadovanovic@users.noreply.github.com>
2020-07-29 16:39:23 +0200

    Add files via upload

---
+++ Nedelja3/Sreda/1.js
@@ -0,0 +1,11 @@
+//Написати функцију која враћа максимум од 3 броја (Параметри функције су 3 броја)
+//MaxOdTri
+
+let x = 25
+let y = -65
+let z = 6
+  function MaxOdTri(x,y,z){
+     return  Math.max(x,y,z)
+  }
+console.log ('Najveći od tri broja je: ' + MaxOdTri(x,y,z))
+
---
+++ Nedelja3/Sreda/2.js
@@ -0,0 +1,9 @@
+//Написати функцију која израчунава цену пице по квадратном центиметру (параметри функције су полупречник у цм,  и цена пице)
+let r =12
+let cena =220
+let P = r*r*Math.PI
+function CenaPoCm(P,cena){
+    return P/cena
+}
+console.log(CenaPoCm(P,cena))
+
